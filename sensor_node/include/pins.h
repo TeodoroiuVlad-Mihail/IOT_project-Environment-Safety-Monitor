6ec3a24d6f85bf74d9901759362b48012408d5fd
Vlad <80387176+TeodoroiuVlad-Mihail@users.noreply.github.com>
2026-01-17 16:55:00 +0200

    Compatibility with Platformio

---
+++ sensor_node/include/pins.h
@@ -0,0 +1,16 @@
+// pins.h
+// Header file defining pin assignments
+
+#pragma once
+
+// I2C
+#define PIN_I2C_SDA   21
+#define PIN_I2C_SCL   22
+
+// Analog inputs (ADC)
+#define PIN_GAS       34   // MQ-7
+#define PIN_HUMIDITY  35   // HR202
+#define PIN_SOUND     32   // Sound sensor: 0104110000082152
+
+// Digital inputs
+#define PIN_VIBRATION 27  // Vibration sensor: 0104110000000347
--- sensor_node/src/main.cpp
+++ sensor_node/src/main.cpp
@@ -9,9 +9,13 @@ void setup() {
     Serial.begin(115200);
     delay(1000);
 
+    Serial.println("Boot OK");
 
     initSensors();
+    Serial.println("Sensors OK");
+
     initWiFi();
+    Serial.println("WiFi OK");
 }
 
 void loop() {
